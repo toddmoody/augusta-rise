c697572d58b6f12ebba5ae615b6a53bfe6c59115
Todd Moody <moods@Todds-MacBook-Pro.local>
2018-02-13 11:40:35 +1300

    add webpack and initial bundle and minification of main js

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    entry: ['./js/functions.js'],
+    output: {
+      filename: './js/build.js',
+      path: __dirname
+    },
+    watch: false,
+    
+        module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          },
+        ],
+        }
+  };
